064e750836a0ba738b45508bcbc132d7bbc99e11
Alejandro Miranda <alex.miranda.sanchez@gmail.com>
2025-02-25 19:46:14 -0600

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,7 +15,7 @@ class TestApp(unittest.TestCase):
     def test_health(self):
         response = self.app.get("/health")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(), "OKi")
+        self.assertEqual(response.data.decode(), "OK")
 
 if __name__ == "__main__":
     unittest.main()
